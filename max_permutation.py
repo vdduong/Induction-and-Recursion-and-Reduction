13c6243aa01491bde8ba586da27ec45eb10ed5cc
vdduong <vdduong@free.fr>
2014-08-18 14:54:30 +0200

    Create max_permutation.py

---
+++ max_permutation.py
@@ -0,0 +1,5 @@
+# each has a favorite seat
+# want to find a way to let them switch seats to make as many people as possible happy with the result
+# they refuse also to move to another seat if they can't get their favorite
+# a form of matching problem
+
