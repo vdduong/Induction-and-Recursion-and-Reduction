027e58023da58cca7e15b0a14ca5d10bf474f562
vdduong <vdduong@free.fr>
2014-08-18 14:20:55 +0200

    Create simple_ex.py

---
+++ simple_ex.py
@@ -0,0 +1,3 @@
+# reducing a problem A to B involves some form of transformation
+# take an example
+# have a list of numbers, want to find the two (non identical) numbers that are closest to each other.
