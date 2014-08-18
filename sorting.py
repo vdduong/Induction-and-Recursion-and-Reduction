0ad19fa45393827857d41aa85b479872f992312b
vdduong <vdduong@free.fr>
2014-08-18 14:36:35 +0200

    Create sorting.py

---
+++ sorting.py
@@ -0,0 +1,2 @@
+# to get the sequence sorted up to position i, first sort it recursively up to position i-1
+# then swap element seq[i] down until it reaches its correct position among the already sorted elements
